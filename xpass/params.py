d648759d531bc5cfb44968c51e7ab159795c9fcf
Arthur Pechkechian <apechkechian@gmail.com>
2023-08-03 20:23:06 +0200

    added a params.py file

---
+++ xpass/params.py
@@ -0,0 +1,6 @@
+import os
+
+STATSBOMB_DATA = os.environ.get("STATSBOMB_DATA")
+THREE_SIXTY = os.path.join(STATSBOMB_DATA, "three-sixty")
+MATCHES = os.path.join(STATSBOMB_DATA, "matches")
+EVENTS = os.path.join(STATSBOMB_DATA, "events")
