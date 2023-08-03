d2fd398c87a71837ac0c8b0a51b9dd50a6715d53
Arthur Pechkechian <apechkechian@gmail.com>
2023-08-03 21:55:43 +0200

    add gender parameter

--- xpass/params.py
+++ xpass/params.py
@@ -4,3 +4,8 @@ STATSBOMB_DATA = os.environ.get("STATSBOMB_DATA")
 THREE_SIXTY = os.path.join(STATSBOMB_DATA, "three-sixty")
 MATCHES = os.path.join(STATSBOMB_DATA, "matches")
 EVENTS = os.path.join(STATSBOMB_DATA, "events")
+
+GENDER = os.environ.get("GENDER")
+
+if __name__ == "__main__":
+    print(GENDER)
