9242ff1c168fd24410a0ac08d44824b9ad3963f3
Arthur Pechkechian <apechkechian@gmail.com>
2023-08-03 21:56:26 +0200

    start loading function

---
+++ xpass/loading.py
@@ -0,0 +1,13 @@
+"""Load the data from the Statsbomb open data folder."""
+
+import json
+import pandas as pd
+
+from xpass.params import STATSBOMB_DATA, THREE_SIXTY, MATCHES, EVENTS, GENDER
+
+def get_data():
+    pass
+
+
+if __name__ == "__main__":
+    print(GENDER)
