96a4db9b36c974b628ca04a0b014460c7b0263ae
Arthur Pechkechian <apechkechian@gmail.com>
2023-08-23 17:09:49 +0200

    add loading from csv file

--- xpass/loading.py
+++ xpass/loading.py
@@ -4,33 +4,37 @@ import os
 import json
 import pandas as pd
 
-from xpass.params import STATSBOMB_DATA, THREE_SIXTY, MATCHES, EVENTS, GENDER
+from xpass.params import PROJECT_HOME, STATSBOMB_DATA, THREE_SIXTY, MATCHES, EVENTS, GENDER
 
 
 def get_data():
     pass
 
 
-def get_competitions(three_sixty: bool = False, gender: str = "All") -> pd.DataFrame:
+def get_competitions() -> pd.DataFrame:
     """Get a DataFrame with the list of competitions available in Statsbomb
     open data.
-
-    Inputs:
-        three_sixty (bool): Set to true to filter on competitions with Statsbomb 360 data
-        gender (str): Set to "male", "female" or "all"
 
     Returns:
         A pandas DataFrame"""
 
-    competitions = pd.read_json(
-        os.path.join(os.path.join(STATSBOMB_DATA, "competitions.json"))
-        )
+    csv_file = os.path.join(PROJECT_HOME, "data", f"competitions_{GENDER}.csv")
 
-    if three_sixty:
+    if os.path.isfile(csv_file):
+        competitions = pd.read_csv(csv_file)
+
+    else:
+
+        competitions = pd.read_json(
+            os.path.join(os.path.join(STATSBOMB_DATA, "competitions.json"))
+            )
+
         competitions = competitions[~competitions["match_available_360"].isnull()]
 
-    if gender in ["male", "female"]:
-        competitions = competitions[competitions["competition_gender"] == gender]
+        if GENDER.lower() in ["male", "female"]:
+            competitions = competitions[competitions["competition_gender"] == GENDER.lower()]
+
+        competitions.to_csv(csv_file, index = False)
 
     return competitions
 
@@ -44,19 +48,28 @@ def get_matches(competitions_df: pd.DataFrame) -> pd.DataFrame:
     Returns:
         A pandas DataFrame"""
 
-    comp_dict = competitions_df[["competition_id", "season_id"]].to_dict("split")
+    csv_file = os.path.join(PROJECT_HOME, "data", f"matches_{GENDER}.csv")
 
-    matches_df_ls = []
-    for competition, season in comp_dict["data"]:
+    if os.path.isfile(csv_file):
+        matches = pd.read_csv(csv_file)
 
-        file = os.path.join(STATSBOMB_DATA, "matches", str(competition), f"{season}.json")
+    else:
 
-        with open(file) as f:
-            data = json.load(f)
-            matches_df  = pd.json_normalize(data, sep = "_")
-            matches_df_ls.append(matches_df)
+        comp_dict = competitions_df[["competition_id", "season_id"]].to_dict("split")
 
-    matches = pd.concat(matches_df_ls).reset_index(drop = True)
+        matches_df_ls = []
+        for competition, season in comp_dict["data"]:
+
+            file = os.path.join(STATSBOMB_DATA, "matches", str(competition), f"{season}.json")
+
+            with open(file) as f:
+                data = json.load(f)
+                matches_df  = pd.json_normalize(data, sep = "_")
+                matches_df_ls.append(matches_df)
+
+        matches = pd.concat(matches_df_ls).reset_index(drop = True)
+
+        matches.to_csv(csv_file, index = False)
 
     return matches
 
@@ -73,57 +86,69 @@ def get_frames_and_events(matches_df: pd.DataFrame) -> tuple:
     Returns:
         A tuple of two pandas DataFrame: (frames, events)"""
 
-    frames_df_ls = []
-    frames_not_found = []
+    csv_file_frames = os.path.join(PROJECT_HOME, "data", f"frames_{GENDER}.csv")
+    csv_file_events = os.path.join(PROJECT_HOME, "data", f"events_{GENDER}.csv")
 
-    for match_id in matches_df["match_id"].unique():
+    if os.path.isfile(csv_file_frames) and os.path.isfile(csv_file_events):
+        frames = pd.read_csv(csv_file_frames)
+        events = pd.read_csv(csv_file_events)
 
-        file = os.path.join(THREE_SIXTY, f"{match_id}.json")
-        try:
-            with open(file) as f:
-                data = json.load(f)
-                frames_df  = pd.json_normalize(data, sep = "_")
-                frames_df_ls.append(frames_df)
-        except:
-            frames_not_found.append(match_id)
+    else:
 
-    frames = pd.concat(frames_df_ls).reset_index(drop = True)
+        frames_df_ls = []
+        frames_not_found = []
 
-    events_df_ls = []
-    events_not_found = []
+        for match_id in matches_df["match_id"].unique():
 
-    for match_id in matches_df["match_id"].unique():
-
-        if match_id in frames_not_found:
-            pass # events have no freeze frames
-
-        else:
-            file = os.path.join(EVENTS, f"{match_id}.json")
+            file = os.path.join(THREE_SIXTY, f"{match_id}.json")
             try:
                 with open(file) as f:
                     data = json.load(f)
-                    events_df  = pd.json_normalize(data, sep = "_")
-                    events_df["match_id"] = match_id
-                    events_df_ls.append(events_df)
+                    frames_df  = pd.json_normalize(data, sep = "_")
+                    frames_df_ls.append(frames_df)
             except:
-                events_not_found.append(match_id)
+                frames_not_found.append(match_id)
 
-    events = pd.concat(events_df_ls).reset_index(drop = True)
+        frames = pd.concat(frames_df_ls).reset_index(drop = True)
 
-    col_origin = [
-        "match_date", "competition_competition_name", "home_team_home_team_gender",
-        "home_team_home_team_name", "away_team_away_team_name",
-    ]
+        events_df_ls = []
+        events_not_found = []
 
-    col_destination = [
-        "match_date", "competition_name", "gender",
-        "home_team_name", "away_team_name"
-    ]
+        for match_id in matches_df["match_id"].unique():
 
-    events[col_destination] = events.apply(
-        lambda x : matches_df[matches_df["match_id"] == x["match_id"]].iloc[0][col_origin],
-        axis = 1, result_type = "expand"
-    )
+            if match_id in frames_not_found:
+                pass # events have no freeze frames
+
+            else:
+                file = os.path.join(EVENTS, f"{match_id}.json")
+                try:
+                    with open(file) as f:
+                        data = json.load(f)
+                        events_df  = pd.json_normalize(data, sep = "_")
+                        events_df["match_id"] = match_id
+                        events_df_ls.append(events_df)
+                except:
+                    events_not_found.append(match_id)
+
+        events = pd.concat(events_df_ls).reset_index(drop = True)
+
+        col_origin = [
+            "match_date", "competition_competition_name", "home_team_home_team_gender",
+            "home_team_home_team_name", "away_team_away_team_name",
+        ]
+
+        col_destination = [
+            "match_date", "competition_name", "gender",
+            "home_team_name", "away_team_name"
+        ]
+
+        events[col_destination] = events.apply(
+            lambda x : matches_df[matches_df["match_id"] == x["match_id"]].iloc[0][col_origin],
+            axis = 1, result_type = "expand"
+        )
+
+        frames.to_csv(csv_file_frames, index = False)
+        events.to_csv(csv_file_events, index = False)
 
     return frames, events
 
@@ -139,34 +164,43 @@ def get_passes(events_df: pd.DataFrame, frames_df: pd.DataFrame) -> pd.DataFrame
     Returns:
         A pandas DataFrame with all the passes and their associated freeze frames"""
 
-    passes = events_df[events_df["type_name"] == "Pass"].reset_index(drop = True)
+    csv_file = os.path.join(PROJECT_HOME, "data", f"passes_{GENDER}.csv")
 
-    columns_to_keep = [
-        "id", "match_date", "competition_name", "gender",
-        "home_team_name", "away_team_name",
-        "index", "period", "timestamp", "minute", "second",
-        "possession", "duration", "type_id", "type_name",
-        "possession_team_id", "possession_team_name", "play_pattern_id",
-        "play_pattern_name", "team_id", "team_name", "related_events",
-        "location", "player_id", "player_name", "position_id",
-        "position_name", "pass_recipient_id", "pass_recipient_name",
-        "pass_length", "pass_angle", "pass_height_id", "pass_height_name",
-        "pass_end_location", "pass_body_part_id", "pass_body_part_name",
-        "pass_type_id", "pass_type_name", "pass_cross", "pass_outcome_id",
-        "pass_outcome_name", "under_pressure", "pass_assisted_shot_id",
-        "pass_shot_assist", "off_camera", "pass_deflected", "counterpress",
-        "pass_aerial_won", "pass_switch", "out", "pass_outswinging",
-        "pass_technique_id", "pass_technique_name", "pass_cut_back",
-        "pass_goal_assist", "pass_through_ball", "pass_miscommunication",
-        "match_id", "pass_no_touch", "pass_straight", "pass_inswinging"
-        ]
+    if os.path.isfile(csv_file):
+        passes = pd.read_csv(csv_file)
 
-    passes = passes[columns_to_keep]
+    else:
 
-    passes = passes.merge(
-        frames_df, how = "left", left_on = "id", right_on = "event_uuid")
+        passes = events_df[events_df["type_name"] == "Pass"].reset_index(drop = True)
 
-    passes = passes[~passes["freeze_frame"].isnull()]
+        columns_to_keep = [
+            "id", "match_date", "competition_name", "gender",
+            "home_team_name", "away_team_name",
+            "index", "period", "timestamp", "minute", "second",
+            "possession", "duration", "type_id", "type_name",
+            "possession_team_id", "possession_team_name", "play_pattern_id",
+            "play_pattern_name", "team_id", "team_name", "related_events",
+            "location", "player_id", "player_name", "position_id",
+            "position_name", "pass_recipient_id", "pass_recipient_name",
+            "pass_length", "pass_angle", "pass_height_id", "pass_height_name",
+            "pass_end_location", "pass_body_part_id", "pass_body_part_name",
+            "pass_type_id", "pass_type_name", "pass_cross", "pass_outcome_id",
+            "pass_outcome_name", "under_pressure", "pass_assisted_shot_id",
+            "pass_shot_assist", "off_camera", "pass_deflected", "counterpress",
+            "pass_aerial_won", "pass_switch", "out", "pass_outswinging",
+            "pass_technique_id", "pass_technique_name", "pass_cut_back",
+            "pass_goal_assist", "pass_through_ball", "pass_miscommunication",
+            "match_id", "pass_no_touch", "pass_straight", "pass_inswinging"
+            ]
+
+        passes = passes[columns_to_keep]
+
+        passes = passes.merge(
+            frames_df, how = "left", left_on = "id", right_on = "event_uuid")
+
+        passes = passes[~passes["freeze_frame"].isnull()]
+
+        passes.to_csv(csv_file, index = False)
 
     return passes
 
@@ -182,18 +216,27 @@ def get_passes_preprocessed(passes_df: pd.DataFrame) -> pd.DataFrame:
 
     """
 
-    passes_df["location_x"] = passes_df["location"].map(lambda x : x[0])
-    passes_df["location_y"] = passes_df["location"].map(lambda x : x[1])
+    csv_file = os.path.join(PROJECT_HOME, "data", f"passes_preprocessed_{GENDER}.csv")
 
-    failure = ["Incomplete", "Out", "Pass Offside"]
-    passes_df["success"] = passes_df["pass_outcome_name"].map(lambda x: int(x not in failure))
+    if os.path.isfile(csv_file):
+        passes_preprocessed = pd.read_csv(csv_file)
 
-    useful_col = [
-        "location_x", "location_y", "play_pattern_name",
-        "pass_angle", "pass_height_id", "pass_body_part_name",
-        "freeze_frame", "success"]
+    else:
 
-    passes_preprocessed = passes_df[useful_col]
+        passes_df["location_x"] = passes_df["location"].map(lambda x : x[0])
+        passes_df["location_y"] = passes_df["location"].map(lambda x : x[1])
+
+        failure = ["Incomplete", "Out", "Pass Offside"]
+        passes_df["success"] = passes_df["pass_outcome_name"].map(lambda x: int(x not in failure))
+
+        useful_col = [
+            "location_x", "location_y", "play_pattern_name",
+            "pass_angle", "pass_height_id", "pass_body_part_name",
+            "freeze_frame", "success"]
+
+        passes_preprocessed = passes_df[useful_col]
+
+        passes_preprocessed.to_csv(csv_file, index = False)
 
     return passes_preprocessed
 
@@ -202,7 +245,7 @@ def get_passes_preprocessed(passes_df: pd.DataFrame) -> pd.DataFrame:
 
 
 if __name__ == "__main__":
-    competitions = get_competitions(three_sixty = True, gender = "female")
+    competitions = get_competitions()
     print("competitions: Done")
     matches = get_matches(competitions)
     print("matches: Done")
@@ -211,3 +254,6 @@ if __name__ == "__main__":
     passes = get_passes(events, frames)
     print("passes: Done")
     print(passes.sample(1).iloc[0])
+    passes_preprocessed = get_passes_preprocessed(passes)
+    print("passes preprocessed: Done")
+    print(passes_preprocessed.sample(1).iloc[0])
--- xpass/params.py
+++ xpass/params.py
@@ -1,5 +1,7 @@
 import os
 
+PROJECT_HOME = os.environ.get("PROJECT_HOME")
+
 STATSBOMB_DATA = os.environ.get("STATSBOMB_DATA")
 THREE_SIXTY = os.path.join(STATSBOMB_DATA, "three-sixty")
 MATCHES = os.path.join(STATSBOMB_DATA, "matches")
