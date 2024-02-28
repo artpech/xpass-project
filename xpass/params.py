9ae94d7de8c2b773c30e409c9bfad497c2ad63b8
Arthur Pechkechian <apechkechian@gmail.com>
2024-02-28 02:45:30 +0100

    add size parameters to enable training on light datasets

--- xpass/params.py
+++ xpass/params.py
@@ -9,5 +9,11 @@ EVENTS = os.path.join(STATSBOMB_DATA, "events")
 
 GENDER = os.environ.get("GENDER")
 
+SIZE = os.environ.get("SIZE")
+SIZE_MAP = {
+    "S" : int(os.environ.get("SIZE_S")),
+    "M" : int(os.environ.get("SIZE_M"))
+}
+
 if __name__ == "__main__":
     print(GENDER)
