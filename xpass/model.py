ee6302f8ed4668fca1df9ca6c7f4c2ae5c981b15
Arthur Pechkechian <apechkechian@gmail.com>
2024-03-06 02:58:38 +0100

    add path and model name parameter to load_model function

--- xpass/model.py
+++ xpass/model.py
@@ -3,7 +3,10 @@ import pickle
 
 from xpass.params import SIZE, PROJECT_HOME
 
-def load_model(model_name: str = f"model_{SIZE}.pkl"):
-    model_path = os.path.join(PROJECT_HOME, "data", "models", model_name)
+def load_model(
+    path: str = os.path.join(PROJECT_HOME, "data", "models"),
+    model_name: str = f"model_{SIZE}.pkl"
+    ):
+    model_path = os.path.join(path, model_name)
     model = pickle.load(open(model_path,"rb"))
     return model
